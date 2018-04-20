21b19656b6f82f40052bf6a64b482e69ee73e988
ymm9527 <yangmingmin2005@126.com>
2018-04-21 06:59:34 +0800

    init

---
+++ utils/config.py
@@ -0,0 +1,17 @@
+class ConfigUtil(object):
+    def __init__(self, config, section="DEFAULT"):
+        self.config = config
+        self.sec = section
+    def get(self, name):
+        return self.config.get(self.sec, name)
+    def get_int(self, name):
+        return self.config.getint(self.sec, name)
+    def get_float(self, name):
+        return self.config.getfloat(self.sec, name)
+    def get_boolean(self, name):
+        return self.config.getboolean(self.sec, name)
+    def items(self):
+        return self.config.items(self.sec)
+    def get_list(self, name, sep=",", dtype=str):
+        lst = self.config.get(self.sec, name).split(",")
+        return [dtype(item) for item in lst]
