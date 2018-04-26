765a16264f2df461baefb7c9fa76b78a7caf9418
飞猹 <feicha.ymm@alibaba-inc.com>
2018-04-26 16:15:55 +0800

    change ptb model config

--- utils/config.py
+++ utils/config.py
@@ -2,16 +2,22 @@ class ConfigUtil(object):
     def __init__(self, config, section="DEFAULT"):
         self.config = config
         self.sec = section
+
     def get(self, name):
         return self.config.get(self.sec, name)
+
     def get_int(self, name):
         return self.config.getint(self.sec, name)
+
     def get_float(self, name):
         return self.config.getfloat(self.sec, name)
+
     def get_boolean(self, name):
         return self.config.getboolean(self.sec, name)
+
     def items(self):
         return self.config.items(self.sec)
+
     def get_list(self, name, sep=",", dtype=str):
-        lst = self.config.get(self.sec, name).split(",")
+        lst = self.config.get(self.sec, name).split(sep)
         return [dtype(item) for item in lst]
